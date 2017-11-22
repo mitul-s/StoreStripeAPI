fa60b341bf6124355b5a0c03059df3e1bef09b28
Mitul Shah <mitulxshah@gmail.com>
2017-11-22 14:09:56 -0500

    fixed typo in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express    = require('express');
-const keys = require('.config/keys');
+const keys = require('./config/keys');
 const stripe     = require('stripe')("keys.stripeSecretKey");
 const bodyParser = require('body-parser');
 const expHandleBars = require("express-handlebars");
