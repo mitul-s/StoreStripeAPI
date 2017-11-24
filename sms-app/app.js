04afe85637b15797bc137f8f80a9ce7385bf52d1
Mitul Shah <mitulxshah@gmail.com>
2017-11-23 19:32:38 -0500

    project complete! 🔥 only if the api wasn't a demo account

--- sms-app/app.js
+++ sms-app/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const nexmo = require('nexmo');
+const Nexmo = require('nexmo');
 const socketio = require('socket.io');
+
+// Initialzie Nexmo API
+const nexmo = new Nexmo({
+    apiKey: '7d506976',
+    apiSecret: 'd2eb538fac815767'
+}, {debug: true});
+
 
 // Initialize application with express
 const app = express();
@@ -23,8 +30,44 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Catch form submit
+app.post('/', (req, res) => {
+    // res.send(req.body);
+    // console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+      '16024919396', number, text, {type: 'unicode'},
+      (err, responseData) => {
+      if(err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+        // Get data from response
+        const data = {
+          id: responseData.messages[0]['message-id'],
+          number: responseData.messages[0]['to']
+        }
+        //Emit data to the client
+        io.emit('smsStatus', data);
+      }
+    }
+    );
+})
+
 // Define a port to start the server
 const port = 3000;
 
 // Start server
 const server = app.listen(port, () => console.log(`Server running on ${port}`));
+
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connect', (socket) => {
+  console.log('Connected fam');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+})
--- sms-app/public/js/main.js
+++ sms-app/public/js/main.js
@@ -5,16 +5,22 @@ const numberInput = document.getElementById('number'),
 
 button.addEventListener('click', send, false);
 
+const socket = io();
+// You're catching data from the app.js emit
+socket.on('smsStatus', function(data){
+    response.innerHTML = '<h5>Text message delivered to ' + data.number + '</h5>';
+})
+
 function send() {
     const number = numberInput.value.replace(/\D/g, '');
-    const message = textInput.value;
+    const text = textInput.value;
 
     fetch('/', {
         method: 'post',
         headers: {
           'Content-type': 'application/json'
         },
-        body: JSON.stringify({number: number, text: text});
+        body: JSON.stringify({number: number, text: text})
     })
     .then(function(res){
         console.log(res);
