267675ded0d7eaa2fa52d3a37064a6e8216cbe62
Mitul Shah <mitulxshah@gmail.com>
2017-12-29 21:47:26 -0500

    built a twitter bot that replies stock prices, some bugs

--- twitterBot/app.js
+++ twitterBot/app.js
@@ -1,86 +1,51 @@
-console.log('Bot is running!');
+console.log('Bot is running.');
 
-/* get() --> search by hashtag, location, user, etc.
-post() --> post a tweet!
-stream() --> a continuous connection, whenever something happens in twitter (@mention), the bot acts */
-
+// Connecting the Twitter API
 const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
 
-// STREAM Request
+const fetch = require('node-fetch');
+var stockPrice;
 
-// // Set up user stream
-// const stream = T.stream('user');
+// Connecting Live Stock Data API
+function getStockData(stock, signal, from) {
+    fetch('https://api.iextrading.com/1.0/stock/'+stock+'/quote')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data.latestPrice);
+        stockPrice = data.latestPrice;
+        if (signal === "$") {
+            console.log(stock);
+            const newTweet = '@' + from + ' Price: $' + stockPrice;
+            tweetIt(newTweet);
+        }
+    })
+    .catch(function(err){
+        console.log("Error: " + err);
+    });
+};
 
-// // Anytime a user follows me, they'll receive a tweet
-// stream.on('follow', followed);
-
-// function followed(eventMsg) {
-//     console.log("Follow event starting");
-//     const name = eventMsg.source.name;
-//     const screenName = eventMsg.source.screen_name;
-//     tweetIt('@' + screenName + ' Thank you for the follow!');
-// }
-
-
-// STREAM Request for Tweets
+// Twitter STREAM Request
 const stream = T.stream('user');
-
 stream.on('tweet', tweetEvent);
 
 function tweetEvent(eventMsg) {
-    // const fs = require('fs');
     const replyto = eventMsg.in_reply_to_screen_name;
     const text = eventMsg.text;
     const from = eventMsg.user.screen_name;
-
-    // This is for a future idea
-    // const signal = text.charAt(replyto.length + 2);
-    // const stock = text.slice(replyto.length + 3, replyto.length + 7);
-    
-    // console.log(replyto + ' ' + from);
-    // console.log(from.length);
-    // console.log(text);
-    // console.log(text.length);
-    // console.log(text.slice(replyto.length + 2, replyto.length + 3));
-
-    // if (signal == '$') {
-    //     console.log(stock);
-    //     const newTweet = '@' + from + ' testing.';
-    //     tweetIt(newTweet);
-    // }
+    const signal = text.charAt(replyto.length + 2);
+    const stock = text.slice(replyto.length + 3, replyto.length + 7);
+    getStockData(stock, signal, from);   
 }
 
-// GET request
-const params = {
-    q: "trump",
-    count: 10
-}
-
-T.get('search/tweets', params, gotData);
-
-function gotData(err, data, response) {
-    const tweets = data.statuses;
-    for(let i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].text)
-    }
-}
-
-// Post Request
-
-// setInterval(tweetIt, 1000 * 20)
 
 function tweetIt(txt) {
 
-    let r = Math.floor(Math.random()*1000);
+    const tweet = {status: txt}
 
-    const tweet = {
-        // status: 'bot tweets ' + r
-        status: txt
-    }
+    T.post('statuses/update', tweet, tweeted);
 
-    T.post('statuses/update', tweet, tweeted)
     function tweeted(err, data, response) {
         if (err) {
             console.log("Something went wrong!");
