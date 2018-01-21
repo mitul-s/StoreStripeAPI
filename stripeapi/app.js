43fa8d2f50687fcd14951f8768dc199892e249e1
Mitul Shah <mitulxshah@gmail.com>
2018-01-21 12:29:18 -0500

    rebuilt stripe api product with ejs

--- stripeapi/app.js
+++ stripeapi/app.js
@@ -1,50 +1,49 @@
-const express    = require('express');
+const express = require('express');
 const keys = require('./config/keys');
-const stripe     = require('stripe')(keys.stripeSecretKey);
+const stripe = require('stripe')(keys.stripeSecretKey);
 const bodyParser = require('body-parser');
-const expHandleBars = require("express-handlebars");
+const ejs = require('ejs');
 
+// Initialize app
 const app = express();
 
-// HANDLEBARS Middleware
-app.engine('handlebars', expHandleBars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-// BODY PARSER Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Static folder
-app.use(express.static(`${__dirname}/public`));
+// View engine setup, middleware
+app.set('view engine', 'ejs');
 
-// INDEX Route
+// Set up static content folder
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-  res.render('index', {
-    stripePublishableKey: keys.stripePublishableKey
-  });
+    res.render('index', {
+        stripePublishableKey: keys.stripePublishableKey
+    });
 });
 
-// Charging route
+
+// Charging Route for Stripe
 app.post('/charge', (req, res) => {
     const amount = 2500;
     // console.log(req.body);
-    // res.send('Test');
     stripe.customers.create({
         email: req.body.stripeEmail,
         source: req.body.stripeToken
     })
     .then(customer => stripe.charges.create({
         amount,
-        description: 'Gone Girl Ebook',
+        description: 'Movie Life Lightroom Presets',
         currency: 'cad',
         customer: customer.id
     }))
     .then(charge => res.render("success"));
 });
 
-
+// Set up port and server
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port}`);
 });
---
+++ stripeapi/old/app.js
@@ -0,0 +1,50 @@
+const express    = require('express');
+const keys = require('./config/keys');
+const stripe     = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const expHandleBars = require("express-handlebars");
+
+const app = express();
+
+// HANDLEBARS Middleware
+app.engine('handlebars', expHandleBars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// BODY PARSER Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Set Static folder
+app.use(express.static(`${__dirname}/public`));
+
+// INDEX Route
+app.get('/', (req, res) => {
+  res.render('index', {
+    stripePublishableKey: keys.stripePublishableKey
+  });
+});
+
+// Charging route
+app.post('/charge', (req, res) => {
+    const amount = 2500;
+    // console.log(req.body);
+    // res.send('Test');
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    })
+    .then(customer => stripe.charges.create({
+        amount,
+        description: 'Gone Girl Ebook',
+        currency: 'cad',
+        customer: customer.id
+    }))
+    .then(charge => res.render("success"));
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
